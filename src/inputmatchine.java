216445687d0436d73428f42a48ed021c34cbbaa7
Raj <rajbhut2832005@gmail.com>
2023-06-03 20:38:07 +0530

    Introdusing new coade

---
+++ src/inputmatchine.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class inputmatchine
+{
+    public static void main(String[] args)
+    {
+     String[] Name;
+        Scanner input =  new Scanner(System.in);
+     while (true)
+     {
+
+
+     }
+
+    }
+}
