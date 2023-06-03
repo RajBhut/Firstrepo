e70fbf9ede128befa07d61eb9eb517122b136311
Henil Malaviya <henilmalaviya06@gmail.com>
2023-06-03 10:20:09 +0530

    refactor: Main.java

--- src/Main.java
+++ src/Main.java
@@ -2,32 +2,16 @@ import java.sql.SQLOutput;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[ ] args)
-    {greating();
+    public static void main(String[ ] args){
         System.out.println("Enter your name");
-String name ;
-Scanner ask = new Scanner(System.in);
-
+        String name;
+        Scanner ask = new Scanner(System.in);
 
         name = ask.next();
         String newname = "Mr. " + name;
 
-        System.out.println("Welcom " + newname);
-
-        System.out.println(nabol);
+        System.out.println("Welcome " + newname);
     }
-
-    public static void greating()
-
-    {
-     String bol = "kam";
-    }
-
-
-
-
-
-public  static String nabol = "nathi";
 
 }
 
